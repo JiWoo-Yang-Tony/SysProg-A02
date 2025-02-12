63db10be8bead597dcafcf1ca4d2f9391ff92e31
Daimon Quin <daimonquin.student@gmail.com>
2025-02-12 04:41:29 -0500

    ASM seems to be working fine. Ive never used stdin/out before but I think I understand it. We need a merge so I know the context better of what it needs to do

--- src/stdIO.c
+++ src/stdIO.c
@@ -1,8 +1,13 @@
 /* FILENAME	: stdIO.c
- * PROGRAMMER	:
+ * PROGRAMMER	: Daimon Quin
  * DATE		: 2025-02-10
  * PURPOSE	: stdIO(standard Input and Output) file holds stdin, stdout function
  */
 
 #include "stdIO.h"
 
+stdIOParse()
+{
+	binaryToAsm(stdin, stdout);
+	//I need more information of how this is being called, we need a merge.
+}
