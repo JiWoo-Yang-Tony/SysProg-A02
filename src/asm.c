f70deacc4f953f68f5cd7cbf7ea1637d92a06053
Daimon Quin <daimonquin.student@gmail.com>
2025-02-12 03:57:27 -0500

    Removed my useless comments and formatted. Fixed spelling errors

--- src/asm.c
+++ src/asm.c
@@ -6,35 +6,20 @@
 
 #include <stdio.h>
 #include "asm.h"
-//Testing for git and making sure vim is doing what I want
 
 void binaryToAsm(FILE *inFp, FILE *outFp)
 {
-	//code go here
-	//Each line requires an fprintf(outFp, "dc.b")
-	//Since each line of asm starts with that??
-	//
-	//While(not over 16 bytes)
-	//Loop and translate each piece into ASM
-	//This needs to be in a bigger loop that does each line
-	//->Needs to fprintf each hex with a ','
-	//
-	//
 	int byteCount = 0;
 	char buffer[16];
-	//Find how many bytes are in the line and add to buffer to go through
-	//which i have no idea how to do
-	//well, maybe a little idea
-	//now im just rambling dont look at my comments, i like comments:
+
 	do
 	{
 		byteCount = fread(buffer, 1, 16, inFp);
-		//i think this loops how i think it does
 		
 		fprintf(outFp, "dc.b\t");
 		for(int i = 0; i < byteCount; i++)
 		{
-			fprintf(fpOut, "%02X%s", buffer[i]);
+			fprintf(outFp, "%02X%s", buffer[i]);
 		
 			if((byteCount-1)>i)
 			{
@@ -42,8 +27,9 @@ void binaryToAsm(FILE *inFp, FILE *outFp)
 			}
 			else
 			{
-				fprintf(fpOut, "\n");
+				fprintf(outFp, "\n");
 			}
 		}	
 	} while(byteCount > 0);
+	return;
 }
